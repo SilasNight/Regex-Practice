a6b42c8d811de440ee4efc53c4e1b8472a67aab0
SilasNight <15emiliomurray@gmail.com>
2025-03-12 08:59:36 +0200

    upload the text file I am using and got the numbers. Made getting the emails actually work properly

--- main.py
+++ main.py
@@ -3,5 +3,10 @@ import re
 with open("Text.txt", "r") as file:
     data = file.read()
 
-emails = re.findall("[^ ]*@.*.com",data)
+
+
+emails = re.findall("[^ ]*@[^ ]*.com",data)
 print(emails)
+
+numbers = re.findall("[ (][0-9]{3}[^a-zA-Z]*[0-9]{3}[^a-zA-Z]*[0-9]{4}[ .]",data)
+print(numbers)
